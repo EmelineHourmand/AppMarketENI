ef522d90dab0c077336ef7a54b81110d2828751d
Emeline <emeline.hourmand@gamil.com>
2019-04-25 16:53:39 +0200

    update bo Article

--- src/fr/eni/appmarketeni/bo/Article.java
+++ src/fr/eni/appmarketeni/bo/Article.java
@@ -2,6 +2,11 @@ package fr.eni.appmarketeni.bo;
 
 import lombok.Data;
 
+/**
+ * 
+ * @author ehourman2019
+ *
+ */
 @Data
 public class Article {
 
@@ -9,4 +14,20 @@ public class Article {
 	private String name;
 	private boolean checked;
 
+	/**
+	 * @param idArticle
+	 * @param name
+	 * @param checked
+	 */
+	public Article(int idArticle, String name, boolean checked) {
+		this.idArticle = idArticle;
+		this.name = name;
+		this.checked = checked;
+	}
+
+	/**
+	 * Empty constructor
+	 */
+	public Article() {
+	}
 }
