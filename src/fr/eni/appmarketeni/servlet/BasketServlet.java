e16981bbf98e4a4443b5e7b5e6aeb3030c9852bf
Aimeric <aimericsylvain@yahoo.fr>
2019-04-26 09:33:15 +0200

    add basket jsp and servlet

---
+++ src/fr/eni/appmarketeni/servlet/BasketServlet.java
@@ -0,0 +1,45 @@
+package fr.eni.appmarketeni.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BasketServlet
+ */
+@WebServlet("/basket")
+public class BasketServlet extends HttpServlet {
+	private static final String PATH_TO_BASKET_JSP = "/WEB-INF/jsp/basket.jsp";
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BasketServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher rd = request.getRequestDispatcher(PATH_TO_BASKET_JSP);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
