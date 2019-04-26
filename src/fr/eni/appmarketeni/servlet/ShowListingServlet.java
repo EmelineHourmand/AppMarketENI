c63f117eb369a6f56d206878e13e76357a7ae47e
Aimeric <aimericsylvain@yahoo.fr>
2019-04-26 11:25:33 +0200

    fix : change servlet url
    Tomcat will work again soon !

--- src/fr/eni/appmarketeni/servlet/ShowListingServlet.java
+++ src/fr/eni/appmarketeni/servlet/ShowListingServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ShowListingServlet
  */
-@WebServlet("/showArticle")
+@WebServlet("/showListing")
 public class ShowListingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
