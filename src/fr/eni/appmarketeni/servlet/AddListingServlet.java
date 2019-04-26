7563b20c89e14e77915b07e7e51fabc04471e3f9
Aimeric <aimericsylvain@yahoo.fr>
2019-04-26 09:32:42 +0200

    add Addlisting jsp and servlet

--- src/fr/eni/appmarketeni/servlet/AddListingServlet.java
+++ src/fr/eni/appmarketeni/servlet/AddListingServlet.java
@@ -1,6 +1,8 @@
 package fr.eni.appmarketeni.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/AddListing")
 public class AddListingServlet extends HttpServlet {
+	private static final String PATH_TO_ADD_LISTING_JSP = "/WEB-INF/jsp/addListing.jsp";
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -27,7 +30,8 @@ public class AddListingServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher(PATH_TO_ADD_LISTING_JSP);
+		rd.forward(request, response);
 	}
 
 	/**
