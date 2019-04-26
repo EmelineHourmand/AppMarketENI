48eb205a8de0a23fb45f049a88167083111271d2
Phanuelle <phanuelle.sainteloi2019@campus-eni.fr>
2019-04-26 10:45:44 +0200

    add ListingManager methods

--- src/fr/eni/appmarketeni/bll/ListingManager.java
+++ src/fr/eni/appmarketeni/bll/ListingManager.java
@@ -1,5 +1,90 @@
 package fr.eni.appmarketeni.bll;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.appmarketeni.bo.Article;
+import fr.eni.appmarketeni.bo.Listing;
+import fr.eni.appmarketeni.dal.dao.DAOFactory;
+import fr.eni.appmarketeni.dal.dao.ListingDAO;
+import fr.eni.appmarketeni.dal.exception.DALException;
+
 public class ListingManager {
 
-}
+	private static ListingDAO daoListing;
+	
+	public ListingManager() {
+		daoListing = DAOFactory.getListingDAO();
+	}
+	
+	public void insertListing(Listing listing) {
+	
+		try {
+			daoListing.insert(listing);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Listing> selectAll(){
+		List<Listing> liste = new ArrayList<Listing>();
+		try {
+			liste = daoListing.selectAll();
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return liste;
+		
+	}
+	
+	public Listing selectById(int id) {
+		Listing liste = new Listing();
+		try {
+			liste = daoListing.selectById(id);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return liste;
+		
+	}
+	
+	public void updateListing(Listing listing){
+		try {
+			daoListing.updateListing(listing);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateArticle(Article article) {
+		try {
+			daoListing.updateArticle(article);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteListing (int id) {
+		try {
+			daoListing.deleteListing(id);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteArticle (int id) {
+		try {
+			daoListing.deleteArticle(id);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+} 
